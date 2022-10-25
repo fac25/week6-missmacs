255656cfe151be33a1eace9351426f41db1626ec
Alex Perez-Davies <alexperezdavies@gmail.com>
2022-10-25 17:38:55 +0100

    Created homepage template

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
-import Head from "next/head";
+import Layout from "../components/layout";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
-  return <div className={styles.container}>Missmacs</div>;
+  return (
+    <Layout home>
+      {/* Nav bar goes here */}
+      <section>
+        <div>Main Body</div>
+      </section>
+    </Layout>
+  );
 }
