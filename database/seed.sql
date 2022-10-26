37388facd5fd53fb663016557fca86ab4d847eed
Alex Perez-Davies <alexperezdavies@gmail.com>
2022-10-26 15:01:29 +0100

    Added onclick and filter by category functionality

--- database/seed.sql
+++ database/seed.sql
@@ -1,8 +1,8 @@
 BEGIN;
 
 INSERT INTO products VALUES
-  (1, 'Mac and Cheese', 'img1.jpg',  7,'main', 'Garfields favourite', 'everyone', 'none'),
-  (2, 'Chocolate cake', 'img2.jpg',  7,'dessert', 'Yassiens favourite', 'everyone', 'none')
+  (1, 'Mac and Cheese', 'img1.jpg',  7,'mains', 'Garfields favourite', 'everyone', 'none'),
+  (2, 'Chocolate cake', 'img2.jpg',  7,'desserts', 'Yassiens favourite', 'everyone', 'none')
 ON CONFLICT DO NOTHING;
 
 COMMIT;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Layout from "../components/layout";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import { getProducts } from "../database/model.js";
+import { useState } from "react";
 
 export async function getStaticProps() {
   // Fetch necessary data for the blog post using params.id
@@ -14,12 +15,58 @@ export async function getStaticProps() {
 }
 
 export default function Home({ products }) {
-  console.log(products);
+  // console.log(products);
+  const [category, setCategory] = useState("all");
+
+  function filterByCategory() {
+    let filtered = products.filter((product) => {
+      return product.category === category;
+    });
+    console.log(filtered);
+
+    return (
+      <ul>
+        {filtered.map((product, index) => {
+          // console.log(product);
+          return <li key={index}>{product.name}</li>;
+        })}
+      </ul>
+    );
+  }
+
+  function handleClick(event) {
+    setCategory(event.target.id);
+  }
+
   return (
     <Layout home>
-      {/* Nav bar goes here */}
+      <nav>
+        <button id="all" onClick={handleClick}>
+          All
+        </button>
+        <button id="starters" onClick={handleClick}>
+          Starters
+        </button>
+        <button id="mains" onClick={handleClick}>
+          Mains
+        </button>
+        <button id="sides" onClick={handleClick}>
+          Sides
+        </button>
+        <button id="desserts" onClick={handleClick}>
+          Desserts
+        </button>
+        <button id="drinks" onClick={handleClick}>
+          Drinks
+        </button>
+      </nav>
       <section>
-        <div>Main Body</div>
+        {/* <div>{category === "all" ? <p>Showing all</p> : ""}</div> */}
+        <div>{filterByCategory()}</div>
+        {/* <div>{category === "mains" ? <p>Showing mains</p> : ""}</div>
+        <div>{category === "sides" ? <p>Showing sides</p> : ""}</div>
+        <div>{category === "desserts" ? <p>Showing desserts</p> : ""}</div>
+        <div>{category === "drinks" ? <p>Showing drinks</p> : ""}</div> */}
       </section>
     </Layout>
   );
