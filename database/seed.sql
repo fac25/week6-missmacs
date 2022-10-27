eba53db892d0271d24ea3f71e77c158d4f25421a
Alex Perez-Davies <alexperezdavies@gmail.com>
2022-10-27 12:51:01 +0100

    Changed seed

--- database/seed.sql
+++ database/seed.sql
@@ -1,11 +1,13 @@
 BEGIN;
 
 INSERT INTO products VALUES
-  (1, ' Mushroom Mac and Cheese', 'img1.jpg',  8,'mains', 'Classic cheese sauce with mushroom', 'dairy, gluten'),
-   (2, 'Chicken Strips', 'img2.jpg',  8,'starters', 'Juicy chicken strips', 'gluten'),
+   (1, 'Chicken Strips', 'img2.jpg',  8,'starters', 'Juicy chicken strips', 'gluten'),
+   (2, ' Mushroom Mac and Cheese', 'img1.jpg',  8,'mains', 'Classic cheese sauce with mushroom', 'dairy, gluten'),
     (3, 'Sloppy Joe Mac and Cheese', 'img3.jpg',  10,'mains', 'Classic cheese sauce with beef ragu', 'dairy, gluten'),
-  (4, 'Chocolate Brownie', 'img4.jpg',  5.50,'desserts', 'Gooey chocolate brownie', 'dairy, gluten'),
-  (5, 'Cheesey Fries', 'img5.jpg',  5.50,'sides', 'Fries loaded with cheese sauce', 'dairy, gluten')
+  (4, 'Cheesey Fries', 'img5.jpg',  5.50,'sides', 'Fries loaded with cheese sauce', 'dairy, gluten'),
+  (5, 'Chocolate Brownie', 'img4.jpg',  5.50,'desserts', 'Gooey chocolate brownie', 'dairy, gluten'),
+  (6, 'Coca Cola', 'coca-cola.jpg',  2.5,'drinks', 'Coca Cola', 'N/a'),
+  (7, 'Moet & Chandon', 'MOET.jpg',  400,'drinks', 'Luxury Champagne ', 'N/a')
 ON CONFLICT DO NOTHING;
 
 COMMIT;
